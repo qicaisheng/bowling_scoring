e754142541cf4cf2a6ac798b1376badd35fc833d
qicaisheng <qicaisheng@qq.com>
2020-07-03 09:46:26 +0800

    extract calculateFrameHitBottles method

--- src/main/java/com/twschool/practice/BowlingScoring.java
+++ src/main/java/com/twschool/practice/BowlingScoring.java
@@ -7,11 +7,13 @@ public class BowlingScoring {
     public int calculate(String scoreString) {
         List<String> frameScores = Arrays.asList(scoreString.split("\\|"));
         
-        return frameScores.stream().mapToInt(frameScore -> {
-            String[] frameScoreArray = frameScore.split("");
-            int firstHitBottleNumber = Integer.parseInt(frameScoreArray[0]);
-            int secondHitBottleNumber = frameScoreArray[1].equals("-") ? 0 : Integer.parseInt(frameScoreArray[1]);
-            return firstHitBottleNumber + secondHitBottleNumber;
-        }).sum();
+        return frameScores.stream().mapToInt(frameScore -> calculateFrameHitBottles(frameScore)).sum();
+    }
+
+    private int calculateFrameHitBottles(String frameScore) {
+        String[] frameScoreArray = frameScore.split("");
+        int firstHitBottleNumber = Integer.parseInt(frameScoreArray[0]);
+        int secondHitBottleNumber = frameScoreArray[1].equals("-") ? 0 : Integer.parseInt(frameScoreArray[1]);
+        return firstHitBottleNumber + secondHitBottleNumber;
     }
 }

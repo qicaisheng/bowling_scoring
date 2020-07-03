f7db6ec20bd638d919001ee3a9f050e9331a1ff7
qicaisheng <qicaisheng@qq.com>
2020-07-03 17:20:51 +0800

    extract BallsFactory

---
+++ src/main/java/com/twschool/practice/BallsFactory.java
@@ -0,0 +1,45 @@
+package com.twschool.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BallsFactory {
+
+    public static List<Ball> buildBalls(String frameScoreString) {
+        int firstHitBottles;
+        int secondHitBottles;
+        List<Ball> balls = new ArrayList<>();
+        String[] frameScoreArray = frameScoreString.split("");
+        if (frameScoreArray[0].equals("X")) {
+            firstHitBottles = 10;
+            balls.add(new StrikeBall());
+        } else if (frameScoreArray[0].equals("-")) {
+            firstHitBottles = 0;
+            balls.add(new NormalBall(0));
+        } else {
+            firstHitBottles = Integer.parseInt(frameScoreArray[0]);
+            balls.add(new NormalBall(firstHitBottles));
+        }
+
+        if (frameScoreArray.length == 2) {
+            if (frameScoreArray[1].equals("X")) {
+                secondHitBottles = 10;
+                balls.add(new StrikeBall());
+            }
+            if (frameScoreArray[1].equals("/")) {
+                secondHitBottles = 10 - firstHitBottles;
+                balls.add(new SpareBall(secondHitBottles));
+            }
+            if (frameScoreArray[1].matches("\\d")) {
+                secondHitBottles = Integer.parseInt(frameScoreArray[1]);
+                balls.add(new NormalBall(secondHitBottles));
+            }
+            if (frameScoreArray[1].equals("-")) {
+                balls.add(new NormalBall(0));
+            }
+        }
+
+        return balls;
+    }
+
+}
--- src/main/java/com/twschool/practice/BowlingFrame.java
+++ src/main/java/com/twschool/practice/BowlingFrame.java
@@ -1,50 +1,16 @@
 package com.twschool.practice;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import static com.twschool.practice.BallsFactory.buildBalls;
+
 public class BowlingFrame {
-    private List<Ball> balls = new ArrayList<>();
+    private List<Ball> balls;
     
     public BowlingFrame(String frameScoreString) {
         balls = buildBalls(frameScoreString);
     }
 
-    private List<Ball> buildBalls(String frameScoreString) {
-        int firstHitBottles;
-        int secondHitBottles;
-        String[] frameScoreArray = frameScoreString.split("");
-        if (frameScoreArray[0].equals("X")) {
-            firstHitBottles = 10;
-            balls.add(new StrikeBall());
-        } else if (frameScoreArray[0].equals("-")) {
-            firstHitBottles = 0;
-            balls.add(new NormalBall(0));
-        } else {
-            firstHitBottles = Integer.parseInt(frameScoreArray[0]);
-            balls.add(new NormalBall(firstHitBottles));
-        }
-
-        if (frameScoreArray.length == 2) {
-            if (frameScoreArray[1].equals("X")) {
-                secondHitBottles = 10;
-                balls.add(new StrikeBall());
-            }
-            if (frameScoreArray[1].equals("/")) {
-                secondHitBottles = 10 - firstHitBottles;
-                balls.add(new SpareBall(secondHitBottles));
-            }
-            if (frameScoreArray[1].matches("\\d")) {
-                secondHitBottles = Integer.parseInt(frameScoreArray[1]);
-                balls.add(new NormalBall(secondHitBottles));
-            }
-            if (frameScoreArray[1].equals("-")) {
-                balls.add(new NormalBall(0));
-            }
-        }
-        
-        return balls;
-    }
 
     public List<Ball> getBalls() {
         return balls;

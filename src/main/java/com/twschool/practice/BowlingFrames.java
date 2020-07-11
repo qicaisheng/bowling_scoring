895aab6d71a63cb196d934a82b24cea13bb14b5a
qicaisheng <qicaisheng@qq.com>
2020-07-11 14:51:29 +0800

    make bowlingFrames field in BowlingFrames final

--- src/main/java/com/twschool/practice/BowlingFrames.java
+++ src/main/java/com/twschool/practice/BowlingFrames.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class BowlingFrames {
-    private List<BowlingFrame> bowlingFrames;
+    private final List<BowlingFrame> bowlingFrames;
 
     public BowlingFrames(List<BowlingFrame> bowlingFrames) {
         this.bowlingFrames = bowlingFrames;

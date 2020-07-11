da6655ca89a202539ebe0182c2e08546e7c7f02a
qicaisheng <qicaisheng@qq.com>
2020-07-11 14:45:46 +0800

    remove unused field in BowlingScoring

--- src/main/java/com/twschool/practice/BowlingScoring.java
+++ src/main/java/com/twschool/practice/BowlingScoring.java
@@ -5,15 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class BowlingScoring {
-    
-    private List<BowlingFrame> bowlingFrames;
+
     private BowlingFrame bonusFrame;
     BowlingFrames allBowlingFrames;
 
     public BowlingScoring(String scoreString) {
         String[] bowlingScoresArray = scoreString.split("\\|\\|");
         List<String> frameScores = Arrays.asList(bowlingScoresArray[0].split("\\|"));
-        bowlingFrames = frameScores.stream().map(BowlingFrame::new).collect(Collectors.toList());
         List<BowlingFrame> newBowlingFrames = frameScores.stream().map(BowlingFrame::new).collect(Collectors.toList());
         boolean withExtraTry = bowlingScoresArray.length == 2;
         if (withExtraTry) {
